8c72ebd936c80cd271d30b56cc0e42dbcc2ddf61
Combatd <combatdstar@gmail.com>
2020-10-06 10:40:17 -0700

    Experiment with Variables in Swift 5

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Lets talk about a Person!
+// What are some attributes of a person?
+var name = "Bob" // String
+var age = 51 // Integer
+var weight = 212.15 // Double
+var isOrganDonor = false // Boolean
+
+print(weight)
+// var is mutable - they can change!
+weight = 200.10
+print(weight)
+// Variables are stored in the RAM on your computer, memory space
+
+// Define constant using let keyword
+let eyeColor = "Blue"
