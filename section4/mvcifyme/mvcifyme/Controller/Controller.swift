ea912346ea24f68a484f7d77336d925858181fae
Combatd <combatdstar@gmail.com>
2020-10-11 13:15:46 -0700

    Add UI View from Objects Library

---
+++ section4/mvcifyme/mvcifyme/Controller/Controller.swift
@@ -0,0 +1,8 @@
+//
+//  Controller.swift
+//  mvcifyme
+//
+//  Created by Mark Calvelo on 10/11/20.
+//
+
+import Foundation
---
+++ section4/mvcifyme/mvcifyme/View/CustomPrettyView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomPrettyView.swift
+//  mvcifyme
+//
+//  Created by Mark Calvelo on 10/11/20.
+//
+
+import UIKit
+
+class CustomPrettyView: UIView {
+    
+
+}
