7956586a8b82c0b03e111f852e2084aa7b4a4a6a
Combatd <combatdstar@gmail.com>
2020-10-11 08:46:31 -0700

    Create XCode Project mvcifyme

---
+++ section4/mvcifyme/mvcifyme/mvcifymeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvcifymeApp.swift
+//  mvcifyme
+//
+//  Created by Mark Calvelo on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct mvcifymeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
