a40245ffb920edd4b291a1bb8e93e7befe418422
Combatd <combatdstar@gmail.com>
2020-10-11 15:10:34 -0700

    Connect View Layer to Controller

--- section4/mvcifyme/mvcifyme/Controller/Controller.swift
+++ section4/mvcifyme/mvcifyme/Controller/Controller.swift
@@ -5,4 +5,20 @@
 //  Created by Mark Calvelo on 10/11/20.
 //
 
-import Foundation
+import UIKit
+
+class Controller: UIViewController {
+    
+    @IBOutlet weak var iphonePriceLabel: UILabel!
+    @IBOutlet weak var iphoneNameLabel: UILabel!
+    @IBOutlet weak var iphoneColorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let appleProduct = AppleProduct(name: "iPhone X", color: "Space Gray", price: 999.99)
+        
+        iphoneNameLabel.text = appleProduct.name
+        iphoneColorLabel.text = "in \(appleProduct.color)"
+        iphonePriceLabel.text = "$\(appleProduct.price)"
+    }
+}
