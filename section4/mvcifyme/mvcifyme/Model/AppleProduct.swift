b5c463005e4ec90443c279989177fc392f0a35f6
Combatd <combatdstar@gmail.com>
2020-10-11 09:12:04 -0700

    Declare model class AppleProduct

---
+++ section4/mvcifyme/mvcifyme/Model/AppleProduct.swift
@@ -0,0 +1,20 @@
+//
+//  AppleProduct.swift
+//  mvcifyme
+//
+//  Created by Mark Calvelo on 10/11/20.
+//
+
+import Foundation
+
+class AppleProduct {
+    var name: String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price: Double) {
+        self.name = name
+        self.color = color
+        self.price = price
+    }
+}
