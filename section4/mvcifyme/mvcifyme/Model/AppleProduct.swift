d81e2618e95c4028d6e7c4e9857cb02238488dad
Combatd <combatdstar@gmail.com>
2020-10-12 02:22:50 -0700

    Declare public properties of AppleProduct as private encapsulated data

--- section4/mvcifyme/mvcifyme/Model/AppleProduct.swift
+++ section4/mvcifyme/mvcifyme/Model/AppleProduct.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 class AppleProduct {
-    var name: String
-    var color: String
-    var price: Double
+    // private variables are only accessible & set inside the same class
+    // public private can access anywhere but only set in the class
+    public private(set) var name: String
+    public private(set) var color: String
+    public private(set) var price: Double
     
     init(name: String, color: String, price: Double) {
         self.name = name
